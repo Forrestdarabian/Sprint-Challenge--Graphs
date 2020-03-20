ed68ece60437d33843d6c1af7d1c7fc527c8374b
Forrest-Darabian <mr.smiley72@yahoo.com>
2020-03-19 18:16:39 -0700

    this should be mvp, need to test some more

--- adv.py
+++ adv.py
@@ -60,7 +60,7 @@ def reverse(direction):
 # Entering a New Room Functionality
 
 
-def new_room(room, visited_rooms):
+def entry(room, visited_rooms):
     visited_rooms[room.id] = {}
     for exit_direction in room.get_exits():
         visited_rooms[room.id][exit_direction] = '?'
@@ -138,18 +138,23 @@ player = Player(world.starting_room)
 traversal_path = []
 visited_rooms = {}
 
-# While the length of visited rooms is less than the length of the entire map
+# While the length of visited rooms is less than the length of the entire map...
 while(len(visited_rooms) < len(room_graph)):
     # This should traverse the player through rooms
     if player.current_room.id not in visited_rooms:
-        new_room(player.current_room, visited_rooms)
+        entry(player.current_room, visited_rooms)
 
+    # Exits is a list
     exits = []
+    # Loop over visited Rooms
     for new_direction in visited_rooms[player.current_room.id]:
+        # This takes care of the ? and appends a new direction to exit
         if (visited_rooms[player.current_room.id][new_direction] == '?'):
             exits.append(new_direction)
 
+    # If the length of exits is 0
     if (len(exits) == 0):
+        # Use our BFS Traversal
         path = breadth_first_search(visited_rooms)
         # Translate Room ID to direction
         for id in path:
@@ -162,11 +167,20 @@ while(len(visited_rooms) < len(room_graph)):
                             exit_direction)
                         visited_rooms[player.current_room.id][exit_direction] = new_room.id
                         if (new_room.id not in visited_rooms):
-                            new_room(new_room, visited_rooms)
+                            entry(new_room, visited_rooms)
                         visited_rooms[new_room.id][reverse(
                             exit_direction)] = player.current_room.id
                         player.travel(exit_direction)
 
+    else:
+        new_exit = random.choice(exits)
+        traversal_path.append(new_exit)
+        new_room = player.current_room.get_room_in_direction(new_exit)
+        visited_rooms[player.current_room.id][new_exit] = new_room.id
+        if (new_room.id not in visited_rooms):
+            entry(new_room, visited_rooms)
+        visited_rooms[new_room.id][reverse(new_exit)] = player.current_room.id
+        player.travel(new_exit)
 
 # TRAVERSAL TEST
 visited_rooms = set()
@@ -185,15 +199,15 @@ else:
     print(f"{len(room_graph) - len(visited_rooms)} unvisited rooms")
 
 
-#######
-# UNCOMMENT TO WALK AROUND
-#######
-player.current_room.print_room_description(player)
-while True:
-    cmds = input("-> ").lower().split(" ")
-    if cmds[0] in ["n", "s", "e", "w"]:
-        player.travel(cmds[0], True)
-    elif cmds[0] == "q":
-        break
-    else:
-        print("I did not understand that command.")
+# #######
+# # UNCOMMENT TO WALK AROUND
+# #######
+# player.current_room.print_room_description(player)
+# while True:
+#     cmds = input("-> ").lower().split(" ")
+#     if cmds[0] in ["n", "s", "e", "w"]:
+#         player.travel(cmds[0], True)
+#     elif cmds[0] == "q":
+#         break
+#     else:
+#         print("I did not understand that command.")
