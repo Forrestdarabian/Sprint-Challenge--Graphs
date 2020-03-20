12ff5497fab97772c3e747a85ae0b215bc0cac1e
Forrest-Darabian <mr.smiley72@yahoo.com>
2020-03-20 10:36:48 -0700

    mvp

--- adv.py
+++ adv.py
@@ -135,51 +135,67 @@ player = Player(world.starting_room)
 # traversal_path = ['n', 'n']
 
 
-traversal_path = []
+traversal_path = ['n', 's', 'e', 'w']
 visited_rooms = {}
 
-# While the length of visited rooms is less than the length of the entire map...
+# while the length of visited rooms is less than the length of the entire map...
 while(len(visited_rooms) < len(room_graph)):
     # This should traverse the player through rooms
     if player.current_room.id not in visited_rooms:
         entry(player.current_room, visited_rooms)
 
-    # Exits is a list
+    # exits is a list
     exits = []
-    # Loop over visited Rooms
+    # loop over visited Rooms
     for new_direction in visited_rooms[player.current_room.id]:
-        # This takes care of the ? and appends a new direction to exit
+        # this takes care of the ? and appends a new direction to exit
         if (visited_rooms[player.current_room.id][new_direction] == '?'):
             exits.append(new_direction)
 
-    # If the length of exits is 0
+    # if the length of exits is 0
     if (len(exits) == 0):
-        # Use our BFS Traversal
+        # use our BFS Traversal
         path = breadth_first_search(visited_rooms)
-        # Translate Room ID to direction
+        # translate Room ID to direction
         for id in path:
             for exit_direction in visited_rooms[player.current_room.id]:
                 if (exit_direction in visited_rooms[player.current_room.id]):
                     # print(f"Current room is {player.current_room.id} and direction is {exit_direction}")
+                    # if the current rooms exit direction is comparable to id and the current rooms id is not equal to ID///
                     if (visited_rooms[player.current_room.id][exit_direction] == id and player.current_room.id != id):
+                        # append the exit direction to our traversal path
                         traversal_path.append(exit_direction)
+                        # set new room equal to the exit direction of the room were currently in
                         new_room = player.current_room.get_room_in_direction(
                             exit_direction)
+                        # then our new room ID is equal to that room
                         visited_rooms[player.current_room.id][exit_direction] = new_room.id
+                        # if new rooms ID isnt in visited rooms...
                         if (new_room.id not in visited_rooms):
+                            # run our entry function to enter a new room
                             entry(new_room, visited_rooms)
+                        # and reverse directions since we need to find a new route
                         visited_rooms[new_room.id][reverse(
                             exit_direction)] = player.current_room.id
+                        # then finally have the player traverse through to the exit
                         player.travel(exit_direction)
 
     else:
+        # set new exit to a random exit
         new_exit = random.choice(exits)
+        # append that new exit to our traversal path
         traversal_path.append(new_exit)
+        # make new room equal to getting the new exit
         new_room = player.current_room.get_room_in_direction(new_exit)
+        # then our exit is equal to our new room ID
         visited_rooms[player.current_room.id][new_exit] = new_room.id
+        # if a new room ID isnt in visited rooms...
         if (new_room.id not in visited_rooms):
+            # run our entry function to enter a new room
             entry(new_room, visited_rooms)
+        # and reverse directions since we need to find a new route
         visited_rooms[new_room.id][reverse(new_exit)] = player.current_room.id
+        # then finally have the player traverse through to the exit
         player.travel(new_exit)
 
 # TRAVERSAL TEST
@@ -202,12 +218,12 @@ else:
 # #######
 # # UNCOMMENT TO WALK AROUND
 # #######
-# player.current_room.print_room_description(player)
-# while True:
-#     cmds = input("-> ").lower().split(" ")
-#     if cmds[0] in ["n", "s", "e", "w"]:
-#         player.travel(cmds[0], True)
-#     elif cmds[0] == "q":
-#         break
-#     else:
-#         print("I did not understand that command.")
+player.current_room.print_room_description(player)
+while True:
+    cmds = input("-> ").lower().split(" ")
+    if cmds[0] in ["n", "s", "e", "w"]:
+        player.travel(cmds[0], True)
+    elif cmds[0] == "q":
+        break
+    else:
+        print("I did not understand that command.")
